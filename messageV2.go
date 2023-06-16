b7cb91c927f0582585872af6030e944b33062d3d
alikonhz <123733442+alikonhz@users.noreply.github.com>
2023-06-16 16:37:01 +0200

    feat: add V2 specific messages With v2 replication protocol Postgres sends additional field xid with some messages during streaming mode. To reflect these changes I added new messages and new function DecodeV2. DecodeV2 in addition to the byte slice accepts inStream parameter. See example on how to use updated API

--- messageV2.go
+++ messageV2.go
@@ -1,1 +1,300 @@
 package pglogrepl
+
+import (
+	"encoding/binary"
+	"time"
+)
+
+type MessageDecoderV2 interface {
+	MessageDecoder
+	DecodeV2(src []byte, inStream bool) error
+}
+
+type StreamStartMessageV2 struct {
+	baseMessage
+
+	Xid uint32
+	// A value of 1 indicates this is the first stream segment for this XID, 0 for any other stream segment
+	FirstSegment uint8
+}
+
+func (m *StreamStartMessageV2) DecodeV2(src []byte, _ bool) (err error) {
+	if len(src) < 5 {
+		return m.lengthError("StreamStartMessageV2", 5, len(src))
+	}
+
+	var low, used int
+	m.Xid, used = m.decodeUint32(src)
+	low += used
+	m.FirstSegment = src[low]
+
+	m.SetType(MessageTypeStreamStart)
+
+	return nil
+}
+
+type StreamStopMessageV2 struct {
+	baseMessage
+}
+
+func (m *StreamStopMessageV2) DecodeV2(_ []byte, _ bool) (err error) {
+	// stream stop has no data
+	m.SetType(MessageTypeStreamStop)
+
+	return nil
+}
+
+type StreamCommitMessageV2 struct {
+	baseMessage
+
+	Xid               uint32
+	Flags             uint8 // currently unused
+	CommitLSN         LSN
+	TransactionEndLSN LSN
+	CommitTime        time.Time
+}
+
+func (m *StreamCommitMessageV2) DecodeV2(src []byte, _ bool) (err error) {
+	if len(src) < 29 {
+		return m.lengthError("StreamCommitMessageV2", 29, len(src))
+	}
+	var low, used int
+	m.Xid, used = m.decodeUint32(src)
+	low += used
+	m.Flags = src[low]
+	low += 1
+	m.CommitLSN, used = m.decodeLSN(src[low:])
+	low += used
+	m.TransactionEndLSN, used = m.decodeLSN(src[low:])
+	low += used
+	m.CommitTime, _ = m.decodeTime(src[low:])
+
+	m.SetType(MessageTypeStreamCommit)
+
+	return nil
+}
+
+type StreamAbortMessageV2 struct {
+	baseMessage
+
+	Xid    uint32
+	SubXid uint32
+}
+
+func (m *StreamAbortMessageV2) DecodeV2(src []byte, _ bool) (err error) {
+	if len(src) < 8 {
+		return m.lengthError("StreamAbortMessageV2", 8, len(src))
+	}
+
+	var low, used int
+	m.Xid, used = m.decodeUint32(src)
+	low += used
+	m.SubXid, _ = m.decodeUint32(src[low:])
+
+	m.SetType(MessageTypeStreamAbort)
+
+	return nil
+}
+
+// ParseV2 parse a logical replication message from protocol version #2
+// it accepts a slice of bytes read from PG and inStream parameter
+// inStream must be true when StreamStartMessage has been read
+// it must be false after StreamStopMessageV2 has been read
+func ParseV2(data []byte, inStream bool) (m Message, err error) {
+	var decoder MessageDecoder
+	msgType := MessageType(data[0])
+
+	switch msgType {
+	case MessageTypeStreamStart:
+		decoder = new(StreamStartMessageV2)
+	case MessageTypeStreamStop:
+		decoder = new(StreamStopMessageV2)
+	case MessageTypeStreamCommit:
+		decoder = new(StreamCommitMessageV2)
+	case MessageTypeStreamAbort:
+		decoder = new(StreamAbortMessageV2)
+	case MessageTypeMessage:
+		decoder = new(LogicalDecodingMessageV2)
+	case MessageTypeRelation:
+		decoder = new(RelationMessageV2)
+	case MessageTypeType:
+		decoder = new(TypeMessageV2)
+	case MessageTypeInsert:
+		decoder = new(InsertMessageV2)
+	case MessageTypeUpdate:
+		decoder = new(UpdateMessageV2)
+	case MessageTypeDelete:
+		decoder = new(DeleteMessageV2)
+	case MessageTypeTruncate:
+		decoder = new(TruncateMessageV2)
+	default:
+		decoder = getCommonDecoder(msgType)
+	}
+
+	if decoder == nil {
+		return nil, errMsgNotSupported
+	}
+
+	if v2, ok := decoder.(MessageDecoderV2); ok {
+		if err = v2.DecodeV2(data[1:], inStream); err != nil {
+			return nil, err
+		}
+	} else if err = decoder.Decode(data[1:]); err != nil {
+		return nil, err
+	}
+
+	return decoder.(Message), nil
+}
+
+type InStreamMessageV2WithXid struct {
+	Xid uint32
+}
+
+type LogicalDecodingMessageV2 struct {
+	LogicalDecodingMessage
+	InStreamMessageV2WithXid
+}
+
+func (m *LogicalDecodingMessageV2) DecodeV2(src []byte, inStream bool) (err error) {
+	if !inStream {
+		return m.LogicalDecodingMessage.Decode(src)
+	}
+
+	if len(src) < 18 {
+		return m.lengthError("LogicalDecodingMessage", 18, len(src))
+	}
+
+	src = readXidAndAdvance(src, &m.InStreamMessageV2WithXid, inStream)
+
+	return m.LogicalDecodingMessage.Decode(src)
+}
+
+type RelationMessageV2 struct {
+	RelationMessage
+	InStreamMessageV2WithXid
+}
+
+func (m *RelationMessageV2) DecodeV2(src []byte, inStream bool) (err error) {
+	if !inStream {
+		return m.RelationMessage.Decode(src)
+	}
+
+	if len(src) < 11 {
+		return m.lengthError("RelationMessageV2", 11, len(src))
+	}
+
+	src = readXidAndAdvance(src, &m.InStreamMessageV2WithXid, inStream)
+
+	return m.RelationMessage.Decode(src)
+}
+
+type TypeMessageV2 struct {
+	TypeMessage
+	InStreamMessageV2WithXid
+}
+
+func (m *TypeMessageV2) DecodeV2(src []byte, inStream bool) (err error) {
+	if !inStream {
+		return m.TypeMessage.Decode(src)
+	}
+
+	if len(src) < 10 {
+		return m.lengthError("TypeMessageV2", 10, len(src))
+	}
+
+	src = readXidAndAdvance(src, &m.InStreamMessageV2WithXid, inStream)
+
+	return m.TypeMessage.Decode(src)
+}
+
+type InsertMessageV2 struct {
+	InsertMessage
+	InStreamMessageV2WithXid
+}
+
+func (m *InsertMessageV2) DecodeV2(src []byte, inStream bool) (err error) {
+	if !inStream {
+		return m.InsertMessage.Decode(src)
+	}
+
+	if len(src) < 12 {
+		return m.lengthError("InsertMessageV2", 12, len(src))
+	}
+
+	src = readXidAndAdvance(src, &m.InStreamMessageV2WithXid, inStream)
+
+	return m.InsertMessage.Decode(src)
+}
+
+type UpdateMessageV2 struct {
+	UpdateMessage
+	InStreamMessageV2WithXid
+}
+
+func (m *UpdateMessageV2) DecodeV2(src []byte, inStream bool) (err error) {
+	if !inStream {
+		return m.UpdateMessage.Decode(src)
+	}
+
+	if len(src) < 10 {
+		return m.lengthError("UpdateMessageV2", 10, len(src))
+	}
+
+	src = readXidAndAdvance(src, &m.InStreamMessageV2WithXid, inStream)
+
+	return m.UpdateMessage.Decode(src)
+}
+
+type DeleteMessageV2 struct {
+	DeleteMessage
+	InStreamMessageV2WithXid
+}
+
+func (m *DeleteMessageV2) DecodeV2(src []byte, inStream bool) (err error) {
+	if !inStream {
+		return m.DeleteMessage.Decode(src)
+	}
+
+	if len(src) < 8 {
+		return m.lengthError("DeleteMessageV2", 8, len(src))
+	}
+
+	src = readXidAndAdvance(src, &m.InStreamMessageV2WithXid, inStream)
+
+	return m.DeleteMessage.Decode(src)
+}
+
+type TruncateMessageV2 struct {
+	TruncateMessage
+	InStreamMessageV2WithXid
+}
+
+func (m *TruncateMessageV2) DecodeV2(src []byte, inStream bool) (err error) {
+	if !inStream {
+		return m.TruncateMessage.Decode(src)
+	}
+
+	if len(src) < 13 {
+		return m.lengthError("TruncateMessageV2", 13, len(src))
+	}
+
+	src = readXidAndAdvance(src, &m.InStreamMessageV2WithXid, inStream)
+
+	return m.TruncateMessage.Decode(src)
+}
+
+func readXidAndAdvance(src []byte, mXid *InStreamMessageV2WithXid, inStream bool) []byte {
+	var xid uint32
+	var used int
+
+	if inStream {
+		xid, used = decodeUint32(src)
+		mXid.Xid = xid
+	}
+
+	return src[used:]
+}
+
+func decodeUint32(src []byte) (uint32, int) {
+	return binary.BigEndian.Uint32(src), 4
+}
---
+++ messageV2_test.go
@@ -0,0 +1,369 @@
+package pglogrepl
+
+import (
+	"errors"
+	"fmt"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+func putMessageTestData(msg []byte, s *messageSuite) *LogicalDecodingMessage {
+	// transaction flag
+	msg[0] = 1
+	off := 1
+
+	lsn := s.newLSN()
+	bigEndian.PutUint64(msg[off:], uint64(lsn))
+	off += 8
+
+	off += s.putString(msg[off:], "test")
+
+	content := "hello"
+
+	bigEndian.PutUint32(msg[off:], uint32(len(content)))
+	off += 4
+
+	for i := 0; i < len(content); i++ {
+		msg[off] = content[i]
+		off++
+	}
+	return &LogicalDecodingMessage{
+		Transactional: true,
+		LSN:           lsn,
+		Prefix:        "test",
+		Content:       []byte("hello"),
+	}
+}
+
+func (s *messageSuite) assertV1NotSupported(msg []byte) {
+	_, err := Parse(msg)
+	s.Error(err)
+	s.True(errors.Is(err, errMsgNotSupported))
+}
+
+func TestLogicalDecodingMessageV2Suite(t *testing.T) {
+	suite.Run(t, new(logicalDecodingMessageSuiteV2))
+}
+
+type logicalDecodingMessageSuiteV2 struct {
+	messageSuite
+}
+
+func (s *logicalDecodingMessageSuiteV2) Test() {
+	msg := make([]byte, 1+4+1+8+5+4+5)
+	msg[0] = 'M'
+	xid := s.newXid()
+	bigEndian.PutUint32(msg[1:], xid)
+
+	expected := putMessageTestData(msg[5:], &s.messageSuite)
+
+	expectedV2 := &LogicalDecodingMessageV2{
+		LogicalDecodingMessage:   *expected,
+		InStreamMessageV2WithXid: InStreamMessageV2WithXid{Xid: xid},
+	}
+	expectedV2.msgType = MessageTypeMessage
+
+	m, err := ParseV2(msg, true)
+	s.NoError(err)
+	logicalDecodingMsg, ok := m.(*LogicalDecodingMessageV2)
+	s.True(ok)
+
+	s.Equal(expectedV2, logicalDecodingMsg)
+}
+
+func TestStreamStartV2Suite(t *testing.T) {
+	suite.Run(t, new(streamStartSuite))
+}
+
+type streamStartSuite struct {
+	messageSuite
+}
+
+func (s *streamStartSuite) Test() {
+	msg := make([]byte, 1+4+1)
+	msg[0] = 'S'
+	xid := s.newXid()
+	firstSeg := byte(1)
+	bigEndian.PutUint32(msg[1:], xid)
+	msg[5] = firstSeg
+
+	expected := &StreamStartMessageV2{
+		Xid:          xid,
+		FirstSegment: firstSeg,
+	}
+	expected.msgType = MessageTypeStreamStart
+	s.assertV1NotSupported(msg)
+
+	m, err := ParseV2(msg, false)
+	s.NoError(err)
+	startMsg, ok := m.(*StreamStartMessageV2)
+	s.True(ok)
+
+	s.Equal(expected, startMsg)
+}
+
+func TestStreamStopV2Suite(t *testing.T) {
+	suite.Run(t, new(streamStopSuite))
+}
+
+type streamStopSuite struct {
+	messageSuite
+}
+
+func (s *streamStopSuite) Test() {
+	msg := make([]byte, 1)
+	msg[0] = 'E'
+
+	expected := &StreamStopMessageV2{}
+	expected.msgType = MessageTypeStreamStop
+
+	s.assertV1NotSupported(msg)
+	m, err := ParseV2(msg, false)
+	s.NoError(err)
+	stopMsg, ok := m.(*StreamStopMessageV2)
+	s.True(ok)
+
+	s.Equal(expected, stopMsg)
+}
+
+func TestStreamCommitV2Suite(t *testing.T) {
+	suite.Run(t, new(streamCommitSuite))
+}
+
+type streamCommitSuite struct {
+	messageSuite
+}
+
+func (s *streamCommitSuite) Test() {
+	msg := make([]byte, 1+4+1+8+8+8)
+	xid := s.newXid()
+	flags := uint8(0)
+	commitLSN := s.newLSN()
+	transactionEndLSN := s.newLSN()
+	commitTime, pgCommitTime := s.newTime()
+
+	msg[0] = 'c'
+	bigEndian.PutUint32(msg[1:], xid)
+	fmt.Printf("%+v\n", msg)
+	msg[5] = flags
+	bigEndian.PutUint64(msg[6:], uint64(commitLSN))
+	bigEndian.PutUint64(msg[14:], uint64(transactionEndLSN))
+	bigEndian.PutUint64(msg[22:], pgCommitTime)
+
+	expected := &StreamCommitMessageV2{
+		Xid:               xid,
+		Flags:             flags,
+		CommitLSN:         commitLSN,
+		TransactionEndLSN: transactionEndLSN,
+		CommitTime:        commitTime,
+	}
+	expected.msgType = MessageTypeStreamCommit
+
+	s.assertV1NotSupported(msg)
+
+	m, err := ParseV2(msg, false)
+	s.NoError(err)
+	streamCommitMsg, ok := m.(*StreamCommitMessageV2)
+	s.True(ok)
+	s.Equal(expected, streamCommitMsg)
+}
+
+func TestStreamAbortV2Suite(t *testing.T) {
+	suite.Run(t, new(streamAbortSuite))
+}
+
+type streamAbortSuite struct {
+	messageSuite
+}
+
+func (s *streamAbortSuite) Test() {
+	msg := make([]byte, 1+4+4)
+
+	xid := s.newXid()
+	subXid := s.newXid()
+
+	msg[0] = 'A'
+	bigEndian.PutUint32(msg[1:], xid)
+	bigEndian.PutUint32(msg[5:], subXid)
+
+	expected := &StreamAbortMessageV2{
+		Xid:    xid,
+		SubXid: subXid,
+	}
+	expected.msgType = MessageTypeStreamAbort
+
+	s.assertV1NotSupported(msg)
+
+	m, err := ParseV2(msg, false)
+	s.NoError(err)
+	streamAbortMsg, ok := m.(*StreamAbortMessageV2)
+	s.True(ok)
+	s.Equal(expected, streamAbortMsg)
+}
+
+func TestRelationMessageV2Suite(t *testing.T) {
+	suite.Run(t, new(relationMessageV2Suite))
+}
+
+type relationMessageV2Suite struct {
+	messageSuite
+}
+
+func (s *relationMessageV2Suite) Test() {
+	msg, expected := s.createRelationTestData()
+
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	relMsg, ok := m.(*RelationMessageV2)
+	s.True(ok)
+	s.Equal(xid, relMsg.Xid)
+	s.Equal(expected, &relMsg.RelationMessage)
+}
+
+func TestTypeMessageV2Suite(t *testing.T) {
+	suite.Run(t, new(typeMessageV2Suite))
+}
+
+type typeMessageV2Suite struct {
+	messageSuite
+}
+
+func (s *typeMessageV2Suite) Test() {
+	msg, expected := s.createTypeTestData()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	typeMsg, ok := m.(*TypeMessageV2)
+	s.True(ok)
+	s.Equal(xid, typeMsg.Xid)
+	s.Equal(expected, &typeMsg.TypeMessage)
+}
+
+func TestInsertMessageV2Suite(t *testing.T) {
+	suite.Run(t, new(insertMessageV2Suite))
+}
+
+type insertMessageV2Suite struct {
+	messageSuite
+}
+
+func (s *insertMessageV2Suite) Test() {
+	msg, expected := s.createInsertTestData()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	insertMsg, ok := m.(*InsertMessageV2)
+	s.True(ok)
+	s.Equal(xid, insertMsg.Xid)
+	s.Equal(expected, &insertMsg.InsertMessage)
+}
+
+func TestUpdateMessageV2Suite(t *testing.T) {
+	suite.Run(t, new(updateMessageV2Suite))
+}
+
+type updateMessageV2Suite struct {
+	messageSuite
+}
+
+func (s *updateMessageV2Suite) TestUpdateV2WithOldTupleTypeK() {
+	msg, expected := s.createUpdateTestDataTypeK()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	updateMsg, ok := m.(*UpdateMessageV2)
+	s.True(ok)
+	s.Equal(xid, updateMsg.Xid)
+	s.Equal(expected, &updateMsg.UpdateMessage)
+}
+
+func (s *updateMessageV2Suite) TestUpdateV2WithOldTupleTypeO() {
+	msg, expected := s.createUpdateTestDataTypeO()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	updateMsg, ok := m.(*UpdateMessageV2)
+	s.True(ok)
+	s.Equal(xid, updateMsg.Xid)
+	s.Equal(expected, &updateMsg.UpdateMessage)
+}
+
+func (s *updateMessageV2Suite) TestUpdateV2WithoutOldTuple() {
+	msg, expected := s.createUpdateTestDataWithoutOldTuple()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	updateMsg, ok := m.(*UpdateMessageV2)
+	s.True(ok)
+	s.Equal(xid, updateMsg.Xid)
+	s.Equal(expected, &updateMsg.UpdateMessage)
+}
+
+func TestDeleteMessageV2Suite(t *testing.T) {
+	suite.Run(t, new(deleteMessageV2Suite))
+}
+
+type deleteMessageV2Suite struct {
+	messageSuite
+}
+
+func (s *deleteMessageV2Suite) TestV2WithOldTupleTypeK() {
+	msg, expected := s.createDeleteTestDataTypeK()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	deleteMsg, ok := m.(*DeleteMessageV2)
+	s.True(ok)
+	s.Equal(xid, deleteMsg.Xid)
+	s.Equal(expected, &deleteMsg.DeleteMessage)
+}
+
+func (s *deleteMessageV2Suite) TestV2WithOldTupleTypeO() {
+	msg, expected := s.createDeleteTestDataTypeO()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	deleteMsg, ok := m.(*DeleteMessageV2)
+	s.True(ok)
+	s.Equal(xid, deleteMsg.Xid)
+	s.Equal(expected, &deleteMsg.DeleteMessage)
+}
+
+func TestTruncateMessageV2Suite(t *testing.T) {
+	suite.Run(t, new(truncateMessageSuiteV2))
+}
+
+type truncateMessageSuiteV2 struct {
+	messageSuite
+}
+
+func (s *truncateMessageSuiteV2) Test() {
+	msg, expected := s.createTruncateTestData()
+	msgV2, xid := s.insertXid(msg)
+
+	m, err := ParseV2(msgV2, true)
+	s.NoError(err)
+	truncateMsg, ok := m.(*TruncateMessageV2)
+	s.True(ok)
+	s.Equal(xid, truncateMsg.Xid)
+	s.Equal(expected, &truncateMsg.TruncateMessage)
+}
+
+func (s *messageSuite) insertXid(msg []byte) ([]byte, uint32) {
+	msgV2 := make([]byte, 4+len(msg))
+	msgV2[0] = msg[0]
+	xid := s.newXid()
+	bigEndian.PutUint32(msgV2[1:], xid)
+	copy(msgV2[5:], msg[1:])
+
+	return msgV2, xid
+}
