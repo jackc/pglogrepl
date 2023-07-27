3749988441a8a099cd055954dac0a129c9d4a1a6
Jonathan Stacks <jonstacks13@gmail.com>
2023-07-27 07:10:51 -0500

    Migrate Travis CI to GitHub Actions

--- travis/before_script.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-psql -U postgres -c 'create database pglogrepl;'
-psql -U postgres -c "create user pglogrepl with replication password 'secret';"
--- travis/script.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-go test -v -race ./...
