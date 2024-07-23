c774291d6ee0c33facdefc798b4828f5fbd8bf0c
David Boon <david.boon@outreach.io>
2024-07-23 10:51:55 -0700

    fix: update handling of CopyData, now returns error
    port upstream fix

--- pglogrepl.go
+++ pglogrepl.go
@@ -657,7 +657,10 @@ func SendStandbyStatusUpdate(_ context.Context, conn *pgconn.PgConn, ssu Standby
 	}
 
 	cd := &pgproto3.CopyData{Data: data}
-	buf := cd.Encode(nil)
+	buf, err := cd.Encode(nil)
+	if err != nil {
+		return err
+	}
 
 	return conn.Frontend().SendUnbufferedEncodedCopyData(buf)
 }
